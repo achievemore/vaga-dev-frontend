93438349232877875b770773c3d620fd8e185291
rwtrecs <adolfo@rewrite.com.br>
2024-10-24 14:33:11 -0400

    feat: header component creation and initialization

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { CommonModule, NgOptimizedImage } from '@angular/common';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [
+    CommonModule,
+    NgOptimizedImage
+  ],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss'
+})
+export class HeaderComponent {
+  get imagePath() {
+    return 'assets/img/image-header.png';
+  }
+}

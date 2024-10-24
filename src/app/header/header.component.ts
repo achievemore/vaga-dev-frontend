9ef4c7981ecc77abfe503b04e367b7d3dd9d2b4c
rwtrecs <adolfo@rewrite.com.br>
2024-10-24 14:43:19 -0400

    feat: header-menu and header-title components creation

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,18 +1,23 @@
 import { Component } from '@angular/core';
 import { CommonModule, NgOptimizedImage } from '@angular/common';
 
+import { HeaderMenuComponent } from './header-menu/header-menu.component';
+import { HeaderTitleComponent } from './header-title/header-title.component';
+
 @Component({
   selector: 'app-header',
   standalone: true,
   imports: [
     CommonModule,
-    NgOptimizedImage
+    NgOptimizedImage,
+    HeaderMenuComponent,
+    HeaderTitleComponent
   ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
-  get imagePath() {
+  get headerImagePath() {
     return 'assets/img/image-header.png';
   }
 }

34432cda6f113b9c03c840c47d5d1d251cc59ee7
rwtrecs <adolfo@rewrite.com.br>
2024-10-25 10:52:38 -0400

    feat: header-main finished

--- src/app/pages/dashboard/header/header-main/header-main.component.ts
+++ src/app/pages/dashboard/header/header-main/header-main.component.ts
@@ -1,9 +1,23 @@
 import { Component } from '@angular/core';
 
+import { NgIconComponent, provideIcons } from '@ng-icons/core';
+import {
+  bootstrapTextCenter,
+  bootstrapPersonFill,
+  bootstrapGearFill,
+  bootstrapBellFill
+} from '@ng-icons/bootstrap-icons';
+
 @Component({
   selector: 'app-pages-dashboard-header-main',
   standalone: true,
-  imports: [],
+  imports: [NgIconComponent],
+  viewProviders: [provideIcons({
+    bootstrapTextCenter,
+    bootstrapPersonFill,
+    bootstrapGearFill,
+    bootstrapBellFill
+  })],
   templateUrl: './header-main.component.html',
   styleUrl: './header-main.component.scss'
 })

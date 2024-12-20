e07d12c88871bae66108409df3faee3a048dfe62
Andre Ofuji <30282153+aofuji@users.noreply.github.com>
2024-12-19 23:37:48 -0300

    Add components pagination

---
+++ src/app/shared/components/pagination/pagination.component.ts
@@ -0,0 +1,20 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-pagination',
+  standalone: true,
+  imports: [],
+  templateUrl: './pagination.component.html',
+  styleUrl: './pagination.component.scss'
+})
+export class PaginationComponent implements OnInit {
+  @Input() currentPage!: number;
+  @Input() itemsPerPage!: number;
+  @Input() totalItems!: number;
+  @Output() pageChanged: EventEmitter<number> = new EventEmitter();
+
+  ngOnInit(): void {
+      console.log(this.currentPage)
+  }
+
+}

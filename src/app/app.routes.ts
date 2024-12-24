6d6a0a83d301515122b8c1f340261eabb9a64493
luluiz21 <luluiz21@hotmail.com>
2024-12-24 00:18:47 -0300

    layout login e nav/sidebar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,29 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './features/dashboard/dashboard.component';
+import { LoginComponent } from './features/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'login',
+        pathMatch: 'full'
+      },
+      {
+        path: 'login',
+        component: LoginComponent
+      },
+      {
+        path: 'dashboard',
+        component: DashboardComponent,
+        /* canActivate: [AuthGuard] */
+      },
+      /* {
+        path: 'users',
+        component: UsersListComponent,
+        canActivate: [AuthGuard]
+      }, */
+      {
+        path: '**',
+        redirectTo: 'login'
+      }
+];
---
+++ src/app/features/dashboard/dashboard.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { HeaderComponent } from './header/header.component';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  standalone: true,
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  imports:[HeaderComponent, SidebarComponent, RouterOutlet],
+  styleUrls: ['./dashboard.component.css']
+})
+export class DashboardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

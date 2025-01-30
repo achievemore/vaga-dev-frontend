ba44c097b22c16a85b70088ccad53dad84a97858
Giovane-Roberti-Tafine <kstv23@hotmail.com>
2025-01-29 21:09:34 -0300

    Inserindo componente menu.

--- src/app/pages/sistema-layout/sistema-layout.page.ts
+++ src/app/pages/sistema-layout/sistema-layout.page.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MenuComponent } from '../../shared/components/menu/menu.component';
 
 @Component({
     selector: 'app-sistema-layout',
     standalone: true,
-    imports: [RouterOutlet],
+    imports: [RouterOutlet, MenuComponent],
     templateUrl: './sistema-layout.page.html',
     styleUrl: './sistema-layout.page.scss'
 })
---
+++ src/app/shared/components/menu/menu.component.ts
@@ -0,0 +1,45 @@
+import { NgClass } from '@angular/common';
+import { Component, inject, Renderer2 } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+
+@Component({
+    selector: 'app-menu',
+    standalone: true,
+    imports: [NgClass, RouterModule],
+    templateUrl: './menu.component.html',
+    styleUrl: './menu.component.scss'
+})
+export class MenuComponent {
+
+    listaMenu = [
+        {
+            title: 'Dashboard',
+            icon: 'icon-house',
+            submenu: [
+                {
+                    title: 'Gerenciar'
+                }
+            ]
+        },
+        {
+            title: 'Usuários',
+            icon: 'icon-squad',
+            submenu: [
+                {
+                    title: 'Listagem',
+                    route: '/sistema/listagem-usuarios'
+                },
+                {
+                    title: 'Cadastrar'
+                }
+            ]
+        }
+    ];
+
+    private renderer2 = inject(Renderer2);
+
+    ativarDesativarSubMenu(index: number): void {
+        this.renderer2.selectRootElement('#box-submenu' + index, true)?.classList.toggle('fechado');
+    }
+}

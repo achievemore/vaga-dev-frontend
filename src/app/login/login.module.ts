07a37e5e5605ebf0261e9aa32fd3ddb958fea941
felipedanilorochasantos <felipe.danilo.rocha.santos@gmail.com>
2025-02-18 19:23:11 +0000

    feat/login

---
+++ src/app/login/login.module.ts
@@ -0,0 +1,15 @@
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LoginComponent } from './login.component';
+import { SharedModule } from '../shared/shared.module';
+
+
+@NgModule({
+  declarations: [LoginComponent],
+  imports: [
+    CommonModule,
+    SharedModule
+  ],
+  exports: [LoginComponent],
+})
+export class LoginModule { }
